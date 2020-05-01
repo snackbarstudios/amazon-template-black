e5a6ae3ef5ec1ee3261d20e1cfa3a8e9f7678779
Fanny Schriwer <fanny_schriwer@hotmail.com>
2020-05-01 10:57:51 +0200

    first commit

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,37 @@
+export default {
+  colors: {
+    text: "#fff",
+    background: "#000",
+    primary: "#970009",
+    highlight: "#D3020C",
+  },
+  breakpoints: ["40em", "56em", "64em"],
+  space: [0, 4, 8, 16, 32, 64, 96, 128, 256, 512],
+  fonts: {
+    body: "Raleway, sans-serif",
+    heading: "Roboto, sans-serif",
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 500,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  letterSpacings: {
+    body: "normal",
+    caps: "0.2em",
+  },
+  useBodyStyles: "true",
+  useBorderBox: "true",
+  styles: {
+    root: {
+      // uses the theme values provided above
+      fontFamily: "body",
+      fontWeight: "body",
+    },
+  },
+}
