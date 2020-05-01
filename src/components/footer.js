46d8dc191bcb2ac6546598bafbb37f36c817a437
lindastorgard <linda.storgard@gmail.com>
2020-05-01 15:17:04 +0200

    add privacy page and footer

---
+++ src/components/footer.js
@@ -0,0 +1,70 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { useStaticQuery, graphql } from "gatsby";
+
+const Footer = () => {
+  const { datoCmsFooter } = useStaticQuery(
+    graphql`
+      query {
+        datoCmsFooter {
+          contactDetailsHeading
+          contactDetails {
+            adressLine
+          }
+          socialMediaHeading
+          socialMedia {
+            title
+            urlLink
+          }
+          privacyHeading
+          privacyTitle
+          copyright
+        }
+      }
+    `
+  );
+
+  const {
+    contactDetailsHeading,
+    contactDetails,
+    socialMediaHeading,
+    socialMedia,
+    privacyHeading,
+    privacyTitle,
+    copyright,
+  } = datoCmsFooter;
+
+  return (
+    <footer>
+      <div sx={{ display: "flex" }}>
+        <div>
+          <p>{contactDetailsHeading}</p>
+          {contactDetails.map(({ adressLine }, index) => (
+            <p key={index}>{adressLine}</p>
+          ))}
+        </div>
+        <div>
+          <p>{socialMediaHeading}</p>
+          {socialMedia.map(({ title, urlLink }, index) => (
+            <p key={index}>
+              {title}
+              {urlLink}
+            </p>
+          ))}
+        </div>
+        <div>
+          <p>{privacyHeading}</p>
+          <p>{privacyTitle}</p>
+        </div>
+      </div>
+      <div>
+        <p>
+          © {new Date().getFullYear()} {copyright}. Powered by
+          Snackbar Studios
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import Footer from "./footer";
+import "./layout.css";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,7 +22,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
@@ -34,18 +35,19 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
+        <Footer />
+        {/* <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </footer> */}
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
