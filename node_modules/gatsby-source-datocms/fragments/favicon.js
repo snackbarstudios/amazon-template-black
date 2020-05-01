15a4467216b78bdd732bc05302394f22411615e7
Fanny Schriwer <fanny_schriwer@hotmail.com>
2020-05-01 14:14:52 +0200

    remove node modules

--- node_modules/gatsby-source-datocms/fragments/favicon.js
+++
@@ -1,8 +0,0 @@
-import { graphql } from 'gatsby';
-
-export const datoCmsFaviconMetaTags = graphql`
-  fragment GatsbyDatoCmsFaviconMetaTags on DatoCmsFaviconMetaTags {
-    tags
-  }
-`
-
--- node_modules/gatsby-source-datocms/fragments/seo.js
+++
@@ -1,7 +0,0 @@
-import { graphql } from 'gatsby';
-
-export const datoCmsSeoMetaTags = graphql`
-  fragment GatsbyDatoCmsSeoMetaTags on DatoCmsSeoMetaTags {
-    tags
-  }
-`
