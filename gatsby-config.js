ee4aae3601bd102dd8f3738e4edbf92df1c66fec
Fanny Schriwer <fanny_schriwer@hotmail.com>
2020-05-01 11:45:40 +0200

    edit gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+let env = process.env.NODE_ENV || "development";
+
+require("dotenv").config({ path: `./.env.${env}` });
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -26,6 +30,14 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATO_API_TOKEN,
+        preview: true,
+        disableLiveReload: false
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
