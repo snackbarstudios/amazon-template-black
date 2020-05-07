a839f92a83cb30d26445b279ed37bf8e90e334ae
lindastorgard <linda.storgard@gmail.com>
2020-05-07 15:36:50 +0200

    add side padding for mobile

--- src/components/mainContainer.js
+++ src/components/mainContainer.js
@@ -4,7 +4,11 @@ import PropTypes from "prop-types";
 
 const MainContainer = ({ children }) => {
   return (
-    <main sx={{ maxWidth: "1140px", margin: "32px auto" }}>{children}</main>
+    <main
+      sx={{ maxWidth: "1140px", margin: "32px auto", px: [3, null, "0px"] }}
+    >
+      {children}
+    </main>
   );
 };
 
