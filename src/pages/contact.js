cb6f0f87653bf637de4ebe4246f2a9c63cbb13a7
lindastorgard <linda.storgard@gmail.com>
2020-05-07 11:24:44 +0200

    restyle contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,49 +2,36 @@
 import { jsx, Styled } from "theme-ui";
 import { useStaticQuery, graphql } from "gatsby";
 import Layout from "../components/layout";
-import PageTitle from "../components/pageTitle";
 import PageContainer from "../components/pageContainer";
 import MainContainer from "../components/mainContainer";
 import Image from "../components/image";
-import OutlineButton from "../components/outlineButton";
 
 const Contact = () => {
-  // const { datoCmsContactPage } = useStaticQuery(
-  //   graphql`
-  //     query {
-  //       datoCmsContactPage {
-  //         phoneNumber
-  //         pageTitle
-  //         companyName
-  //         address {
-  //           addressLine
-  //         }
-  //         email
-  //         mapButtonText
-  //         mapButtonLink {
-  //           latitude
-  //           longitude
-  //         }
-  //         image {
-  //           alt
-  //           fluid {
-  //             ...GatsbyDatoCmsFluid
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // );
-  // const {
-  //   phoneNumber,
-  //   pageTitle,
-  //   companyName,
-  //   address,
-  //   email,
-  //   mapButtonText,
-  //   mapButtonLink,
-  //   image,
-  // } = datoCmsContactPage;
+  const { datoCmsContactPage } = useStaticQuery(
+    graphql`
+      query {
+        datoCmsContactPage {
+          phoneNumber
+          pageTitle
+          companyName
+          email
+          image {
+            alt
+            fluid {
+              ...GatsbyDatoCmsFluid
+            }
+          }
+        }
+      }
+    `
+  );
+  const {
+    phoneNumber,
+    pageTitle,
+    companyName,
+    email,
+    image,
+  } = datoCmsContactPage;
 
   const style = {
     paragraph: {
@@ -64,52 +51,55 @@ const Contact = () => {
       },
     },
     image: {
-      overflow: "hidden",
+      width: "100%",
+      height: "100%",
       mb: [4, "0px", null],
+      div: {
+        width: "100%",
+        height: "100%",
+      },
     },
   };
 
   return (
     <Layout>
-      {/* <MainContainer>
-        <PageTitle>{pageTitle}</PageTitle>
+      <MainContainer>
         <PageContainer>
           <div
             sx={{
               display: "flex",
               flexDirection: ["column-reverse", "row", null],
+              width: "100%",
+              height: "100%",
             }}
           >
-            <div sx={{ flex: 1, pr: ["0px", 4, null], pt: ["0px", null, 4] }}>
-              <Styled.h2>{companyName}</Styled.h2>
-              {address.map(({ addressLine }, index) => (
-                <p key={index} sx={style.paragraph}>
-                  {addressLine}
-                </p>
-              ))}
-              <div sx={{ mt: 4, mb: [4, null, 5] }}>
-                <a href={`tel:${phoneNumber}`} sx={style.a}>
-                  {phoneNumber}
-                </a>
-                <a href={`mailto:${email}`} sx={style.a}>
-                  {email}
-                </a>
+            <div
+              sx={{
+                flex: 1,
+                backgroundColor: "primary",
+              }}
+            >
+              <div sx={{ p: 5 }}>
+                <Styled.h2>{pageTitle}</Styled.h2>
+                <p>{companyName}</p>
+                <div sx={{ mt: 4 }}>
+                  <a href={`tel:${phoneNumber}`} sx={style.a}>
+                    {phoneNumber}
+                  </a>
+                  <a href={`mailto:${email}`} sx={style.a}>
+                    {email}
+                  </a>
+                </div>
               </div>
-              <OutlineButton
-                text={mapButtonText}
-                target="_blank"
-                rel="noreferrer noopener"
-                href={`https://www.google.com/maps/search/?api=1&query${mapButtonLink.latitude}${mapButtonLink.longitude}`}
-              />
             </div>
-            <div sx={{ flex: 2 }}>
+            <div sx={{ flex: 1 }}>
               <div sx={style.image}>
                 <Image image={image.fluid} />
               </div>
             </div>
           </div>
         </PageContainer>
-      </MainContainer> */}
+      </MainContainer>
     </Layout>
   );
 };
