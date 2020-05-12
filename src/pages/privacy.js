6df1b0b530fb1820dc244b3ae80d93ef47a184c2
lindastorgard <linda.storgard@gmail.com>
2020-05-12 11:46:42 +0200

    test

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -32,7 +32,7 @@ const PrivacyPage = () => {
           <div sx={{ color: "highlight" }}>
             <Styled.h2>{pageTitle}</Styled.h2>
           </div>
-          <div
+          <p
             sx={{
               py: 5,
               h1: {
